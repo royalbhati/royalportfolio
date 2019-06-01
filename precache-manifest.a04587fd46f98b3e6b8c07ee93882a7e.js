3111810a3f19447b6c3a8947548a3e585d743f69
Royal Bhati <royal240596@gmail.com>
2019-06-01 12:12:07 +0530

    Updates

---
+++ precache-manifest.a04587fd46f98b3e6b8c07ee93882a7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8cecf6daa634b6911bf",
+    "url": "/royalportfolio/static/css/main.7f486725.chunk.css"
+  },
+  {
+    "revision": "e8cecf6daa634b6911bf",
+    "url": "/royalportfolio/static/js/main.e8cecf6d.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/royalportfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "7c6cca8a0f9c4a0ba881",
+    "url": "/royalportfolio/static/js/runtime~main.7c6cca8a.js"
+  },
+  {
+    "revision": "fb4c5cfce9fc75339e21173a6f9a922c",
+    "url": "/royalportfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be42516f3a87baf036d8fb667a6d8098.js"
+  "/royalportfolio/precache-manifest.a04587fd46f98b3e6b8c07ee93882a7e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/royalportfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
