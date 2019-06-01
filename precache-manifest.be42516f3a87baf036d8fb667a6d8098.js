72b3262ca48575ffde0c309d131eaa4594c1ec21
Royal Bhati <royal240596@gmail.com>
2019-06-01 11:59:04 +0530

    Updates

---
+++ precache-manifest.be42516f3a87baf036d8fb667a6d8098.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70fd8c90487577e199b5",
+    "url": "/static/css/main.647cc650.chunk.css"
+  },
+  {
+    "revision": "70fd8c90487577e199b5",
+    "url": "/static/js/main.70fd8c90.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1bf42e685d269e3f2315d173d49722bb",
+    "url": "/index.html"
+  }
+];
